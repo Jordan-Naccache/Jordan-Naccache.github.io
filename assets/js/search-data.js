cb7ac1d01d7ff93ea467e3d324cb100d6c0bf2e8
Jeth1n <166243434+Jeth1n@users.noreply.github.com>
2025-03-19 14:24:16 +0000

    Deploying to gh-pages from @ Jordan-Naccache/Jordan-Naccache.github.io@f6e00416277d13056dddfbdc79e4af0a840e6a30 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "For the complete version of my CV, please refer to the attached PDF on the right.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
